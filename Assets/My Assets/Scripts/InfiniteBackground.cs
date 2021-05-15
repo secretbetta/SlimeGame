af0a6b4a3bf87de7ee20784c254f931b432d8fa5
Andrew Nguyen <secretbetta@gmail.com>
2021-05-15 02:12:19 -0700

    Researching infinite vertical scroll

---
+++ Assets/My Assets/Scripts/InfiniteBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfiniteBackground : MonoBehaviour {
+
+	private BoxCollider2D boxCollider;
+
+	private Rigidbody2D rb;
+
+	private float width;
+
+	private float speed = -3f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
