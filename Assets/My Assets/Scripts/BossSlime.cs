d05a46d0393a98a2c593451a540df9304fa4598d
Andrew Nguyen <secretbetta@gmail.com>
2021-05-16 18:40:43 -0700

    Adding BossSlime functionality

---
+++ Assets/My Assets/Scripts/BossSlime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossSlime : MonoBehaviour
+{
+    private Rigidbody2D myrigidbody;
+    public Vector3 destination = Vector3.zero;
+
+    void Awake()
+    {
+        myrigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.tag != "Player") return;
+        myrigidbody.AddForce(transform.up * 15, ForceMode2D.Impulse);
+        StartCoroutine(BossCoroutine());
+    }
+
+    IEnumerator BossCoroutine() {
+        yield return new WaitForSecondsRealtime(2);
+        transform.position = destination;
+    }
+}
--- Assets/My Assets/Scripts/MoveBetween.cs
+++ Assets/My Assets/Scripts/MoveBetween.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class MoveBetween : MonoBehaviour
 {
+    private Transform ThisTransform = null;
+    public Vector3 startPos = Vector3.zero;
+    public Vector3 endPos = Vector3.zero;
+    public float speed = 0.5f;
     // Start is called before the first frame update
-    void Start()
+    void Awake ()
     {
-        
+        ThisTransform = GetComponent<Transform>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        print(Mathf.MoveTowards(-7, 7, speed));
+        ThisTransform.position.Set(Mathf.MoveTowards(startPos.x, endPos.x, speed), ThisTransform.position.y, ThisTransform.position.z);
+    }
+
+    private void pingPong() {
+        ThisTransform.position.Set(Mathf.MoveTowards(startPos.x, endPos.x, speed), ThisTransform.position.y, ThisTransform.position.z);
     }
 }
