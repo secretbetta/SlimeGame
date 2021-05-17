6f04cf6288576b3e6c018632fd7d993547f32bcb
Andrew Nguyen <secretbetta@gmail.com>
2021-05-17 01:41:30 -0700

    Finished Boss functionality

--- Assets/My Assets/Scripts/BossSlime.cs
+++ Assets/My Assets/Scripts/BossSlime.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BossSlime : MonoBehaviour
 {
     private Rigidbody2D myrigidbody;
+    private bool runaway = false;
     public Vector3 destination = Vector3.zero;
+    public Text dialogue;
 
     void Awake()
     {
@@ -20,12 +23,20 @@ public class BossSlime : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag != "Player") return;
-        myrigidbody.AddForce(transform.up * 15, ForceMode2D.Impulse);
-        StartCoroutine(BossCoroutine());
+        if (!runaway) {
+            myrigidbody.AddForce(transform.up * 15, ForceMode2D.Impulse);
+            StartCoroutine(BossCoroutine());
+            print("Finish Routine");
+            runaway = true;
+        }
     }
 
     IEnumerator BossCoroutine() {
         yield return new WaitForSecondsRealtime(2);
         transform.position = destination;
+        dialogue.text = "Nooo you got me! Not my treasure!";
+        Vector3 scaleChange = transform.GetChild(0).localScale;
+        scaleChange.x *= -1;
+        transform.GetChild(0).localScale = scaleChange;
     }
 }
