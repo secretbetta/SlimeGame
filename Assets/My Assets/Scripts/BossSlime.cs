e8e6549644edad9e3877f7048ebce6a4482bc5fb
Andrew Nguyen <secretbetta@gmail.com>
2021-05-17 14:20:17 -0700

    Completed boss functionality with NPC spawner for help

--- Assets/My Assets/Scripts/BossSlime.cs
+++ Assets/My Assets/Scripts/BossSlime.cs
@@ -7,6 +7,7 @@ public class BossSlime : MonoBehaviour
 {
     private Rigidbody2D myrigidbody;
     private bool runaway = false;
+    public GameObject NPC;
     public Vector3 destination = Vector3.zero;
     public Text dialogue;
 
@@ -16,27 +17,29 @@ public class BossSlime : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter2D(Collision2D collision) {
         if (collision.gameObject.tag != "Player") return;
         if (!runaway) {
             myrigidbody.AddForce(transform.up * 15, ForceMode2D.Impulse);
             StartCoroutine(BossCoroutine());
-            print("Finish Routine");
             runaway = true;
         }
     }
 
     IEnumerator BossCoroutine() {
+        GameObject NPCHelper = GameObject.Instantiate(NPC);
+        NPCHelper.GetComponentInChildren<Text>().text = "He's getting away! Get to the top fast!";
+        NPCHelper.SetActive(false);
+        NPCHelper.transform.position = gameObject.transform.position;
+        NPCHelper.transform.position += new Vector3(-2, 0, 10);
         yield return new WaitForSecondsRealtime(2);
         transform.position = destination;
         dialogue.text = "Nooo you got me! Not my treasure!";
         Vector3 scaleChange = transform.GetChild(0).localScale;
         scaleChange.x *= -1;
         transform.GetChild(0).localScale = scaleChange;
+        NPCHelper.SetActive(true);
+
     }
 }
